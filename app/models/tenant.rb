38a4918b89fe8eb1ac428ef7e0e25a8dbceed7b5
Kevin Jing Qiu <kevin@idempotent.ca>
2018-03-04 10:39:31 -0500

    Use a more specific error type

--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -2,8 +2,8 @@ class Tenant < ApplicationRecord
   after_create do
     begin
       Apartment::Tenant.create(name)
-    rescue StandardError => e
-      #puts e
+    rescue Apartment::TenantExists => e
+      puts "Tenant #{name} already exists. Skipping..."
     end
   end
 end
