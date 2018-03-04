40ab27de91cb10229443288a30b1c79a7398dd10
Kevin Jing Qiu <kevin@idempotent.ca>
2018-03-04 11:37:45 -0500

    Tests for the tenant model

--- test/models/tenant_test.rb
+++ test/models/tenant_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class TenantTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "create tenant model should create a apartment tenant" do
+    Tenant.create({name: "test_subdomain"})
+    tenants = Apartment::Tenant.each {}
+    assert tenants.include? "test_subdomain"
+  end
+
+  test "create multiple tenants" do
+    Tenant.create([{name: "alpha"}, {name: "beta"}])
+    tenants = Apartment::Tenant.each {}
+    assert_equal tenants, ["alpha", "beta"]
+  end
 end
