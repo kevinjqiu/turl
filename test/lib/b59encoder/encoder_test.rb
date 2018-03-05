c30cf4b35700bffcf35537434f5c2b3074ec9fc7
Kevin Jing Qiu <kevin@idempotent.ca>
2018-03-05 13:05:50 -0500

    Unskip tests

--- test/lib/b59encoder/encoder_test.rb
+++ test/lib/b59encoder/encoder_test.rb
@@ -8,17 +8,14 @@ class TokenizerTest < ActiveSupport::TestCase
   end
 
   test "9 is encoded to 9" do
-    skip
     assert_equal "9", b59encode(9)
   end
 
   test "58 is encoded to _" do
-    skip
     assert_equal "_", b59encode(58)
   end
 
   test "59 is encoded to 10" do
-    skip
     assert_equal "10", b59encode(59)
   end
 
