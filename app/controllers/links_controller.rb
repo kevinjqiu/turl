9eb24c1788e038dfa14f463201e61c639dd5fcba
Kevin Jing Qiu <kevin@idempotent.ca>
2018-03-04 21:16:17 -0500

    Write tokenizer

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -4,11 +4,23 @@ class LinksController < ApplicationController
 
   def create
     @link = Link.create!(link_params)
-    json(@link)
+    @link[:shortened] = tokenizer.tokenize(@link[:original])
+    json(@link, :created)
   end
 
   private
   def link_params
-    params.require(:link).permit(:original)
+    params.require(:link).permit(:original).reject(:shortened).reject(:id)
+  end
+
+  def tokenizer
+    return Tokenizer.new
+  end
+end
+
+class Tokenizer
+  ALPHABET = Array("0".."9") + Array("a".."z") + Array("A".."Z") + ["-", "_"] - ["o", "O", "i", "I", "l"]
+
+  def tokenize(n)
   end
 end
--- test/controllers/links_controller_test.rb
+++ test/controllers/links_controller_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class LinksControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  setup do
+    Tenant.create({name: "alpha"})
+  end
+
+  teardown do
+    Apartment::Tenant.drop("alpha")
+  end
+
+  test "shorten a simple link" do
+    Apartment::Tenant.switch! "alpha"
+    post_json 'http://alpha.lvh.me/links', { original: 'http://www.google.com' }
+    assert_response :created
+    assert_not response_json[:shortened].nil?
+  end
 end
